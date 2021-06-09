93bd64f1a04b9d0321ffd63f455217f152a3b41f
agrem <57567164+agrem@users.noreply.github.com>
2021-06-10 00:39:36 +0200

    szl

---
+++ szl.h/szl.h
@@ -0,0 +1,18 @@
+#ifndef szl_h
+#define szl_h
+//standard zjeb library
+
+#define samochod auto
+#define mapka map
+#define cztery for
+#define wieloryb while
+#define erekcja stoi
+#define wektor vector
+#define dodaj +
+#define oddaj -
+#define kopuluj *
+#define nadwor cout
+#define naobiad cin
+#define ukladaj sort
+
+#endif
